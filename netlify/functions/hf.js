a9c4d7fe40da316f3bd3bbd226aad96d6f230057
simplanimulo <galwanizator@gmial.com>
2026-05-31 19:02:50 +0200

    qt

--- netlify/functions/hf.js
+++ netlify/functions/hf.js
@@ -4,7 +4,7 @@ exports.handler = async (event) => {
     console.log('Received prompt:', event.body);
     const { prompt } = JSON.parse(event.body);
     //const HF_URL = 'https://api-inference.huggingface.co/models/gpt2';
-    const HF_URL = 'https://router.huggingface.co/hf-inference/models/gpt2';
+    const HF_URL = 'https://router.huggingface.co/hf-inference/models/gpt2-xl';
     //const HF_URL = 'https://jsonplaceholder.typicode.com/posts';
     
     const response = await fetch(HF_URL, {
