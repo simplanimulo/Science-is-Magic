38670fc2c2182269f345ebf99f49140cfd54fbcd
simplanimulo <galwanizator@gmial.com>
2026-05-31 17:58:19 +0200

    t3

--- netlify/functions/testf.js
+++ netlify/functions/testf.js
@@ -2,6 +2,6 @@ export const handler = async (event, context) => {
     console.log('test function starts');
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello from Netlify Functions!" }),
+    body: JSON.stringify({ message: "Hello from Netlify Functionss!" }),
   };
 };
